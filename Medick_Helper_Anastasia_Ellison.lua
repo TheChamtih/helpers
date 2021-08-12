8da13a275d53ebb03c9795953db58d0736091a08
TheChamtih <75412087+TheChamtih@users.noreply.github.com>
2021-08-12 14:14:09 +0500

    Add files via upload

--- Medick_Helper_Anastasia_Ellison.lua
+++ Medick_Helper_Anastasia_Ellison.lua
@@ -1,5 +1,5 @@
 script_name('Medick Helper')
-script_version '3.8.0'
+script_version '3.8.1'
 local dlstatus = require "moonloader".download_status
 script_author('Doni_Baerra,Makar_Sheludkov')
 local sf = require 'sampfuncs'
